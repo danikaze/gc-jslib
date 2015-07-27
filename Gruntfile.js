bb349e0c88548b46c6e83efc8c7d0751640e49ac
danikaze <danikaze@gmail.com>
2015-07-27 20:12:44 +0900

    Added Grunt

---
+++ Gruntfile.js
@@ -0,0 +1,51 @@
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        clean : {
+            docs: ['docs'],
+            build: ['build'],
+            default: ['docs', 'build']
+        },
+
+        jsdoc : {
+            build : {
+                src: [
+                    'src/*.js',
+                    'README.md',
+                    '!src/_template_.js',
+                    '!src/polyfill.js'
+                ],
+                options: {
+                    destination: 'docs',
+                    configure: 'jsdoc-config.json'
+                }
+            }
+        },
+
+        uglify: {
+            options: {
+                screwIE8: true,
+                sourceMap: true,
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd HH:MM:ss") %> */'
+            },
+            build: {
+                files: {
+                    'build/<%= pkg.name %>.min.js': [
+                        'src/*.js',
+                        '!src/_template_.js'
+                    ]
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-jsdoc');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    // Default task(s).
+    grunt.registerTask('default', ['clean:docs', 'jsdoc', 'uglify']);
+};
