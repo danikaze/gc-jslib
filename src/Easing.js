089a4a7e1cb1cba11bb02e7b70b4a208fce41215
danikaze <danikaze@gmail.com>
2015-08-01 22:51:12 +0900

    ELASTIC easing functions fixed

--- src/Easing.js
+++ src/Easing.js
@@ -534,8 +534,8 @@
     {
         var s=1.70158, p=0, a=c;
         if (t==0) return b;  if ((t/=d)==1) return b+c;  if (!p) p=d*.3;
-        if (a < Math.abs(c)) { a=c; var s=p/4; }
-        else var s = p/(2*Math.PI) * Math.asin (c/a);
+        if (a < Math.abs(c)) { a=c; s=p/4; }
+        else s = p/(2*Math.PI) * Math.asin (c/a);
         return -(a*Math.pow(2,10*(t-=1)) * Math.sin( (t*d-s)*(2*Math.PI)/p )) + b;
     }
 
@@ -553,11 +553,11 @@
      */
     function easeOutElastic(t, b, c, d)
     {
-        var s=1.70158;var p=0;var a=c;
+        var s=1.70158, p=0, a=c;
         if (t==0) return b;  if ((t/=d)==1) return b+c;  if (!p) p=d*.3;
-        if (a < Math.abs(c)) { a=c; var s=p/4; }
-        else var s = p/(2*Math.PI) * Math.asin (c/a);
-        return -(a*Math.pow(2,10*(t-=1)) * Math.sin( (t*d-s)*(2*Math.PI)/p )) + b;
+        if (a < Math.abs(c)) { a=c; s=p/4; }
+        else s = p/(2*Math.PI) * Math.asin (c/a);
+        return a*Math.pow(2,-10*t) * Math.sin( (t*d-s)*(2*Math.PI)/p ) + c + b;
     }
 
     /**
@@ -574,10 +574,10 @@
      */
     function easeInOutElastic(t, b, c, d)
     {
-        var s=1.70158;var p=0;var a=c;
+        var s=1.70158, p=0, a=c;
         if (t==0) return b;  if ((t/=d/2)==2) return b+c;  if (!p) p=d*(.3*1.5);
-        if (a < Math.abs(c)) { a=c; var s=p/4; }
-        else var s = p/(2*Math.PI) * Math.asin (c/a);
+        if (a < Math.abs(c)) { a=c; s=p/4; }
+        else s = p/(2*Math.PI) * Math.asin (c/a);
         if (t < 1) return -.5*(a*Math.pow(2,10*(t-=1)) * Math.sin( (t*d-s)*(2*Math.PI)/p )) + b;
         return a*Math.pow(2,-10*(t-=1)) * Math.sin( (t*d-s)*(2*Math.PI)/p )*.5 + c + b;
     }
