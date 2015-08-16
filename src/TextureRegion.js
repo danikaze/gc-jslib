972e4dc9e5707cc0cb4e82ba5b4d2ad09bb624b4
danikaze <danikaze@gmail.com>
2015-08-16 20:23:47 +0900

    gc.TextureRegion accepts just an image now
    If only an image is provided, all the other parameters are optionals
    being the TextureRegion the whole image (centered in its top-left
    corner)

--- src/TextureRegion.js
+++ src/TextureRegion.js
@@ -13,25 +13,27 @@
      * @memberOf gc.TextureRegion
      * @public
      */
-    var VERSION = "1.0.0";
+    var VERSION = "0.2.0";
 
     /**
      * Description of the class
      *
-     * @param {Object}  texture Source of the image
-     * @param {Integer} x       x-position of the start of the region
-     * @param {Integer} y       y-position of the start of the region
-     * @param {Integer} w       Width of the start of the region
-     * @param {Integer} h       height of the start of the region
+     * @param {Object}  texture                 Source of the image
+     * @param {Integer} [offsetX=0]             X-position of the start of the region
+     * @param {Integer} [offsetY=0]             Y-position of the start of the region
+     * @param {Integer} [width=texture.width]   Width of the start of the region
+     * @param {Integer} [height=texture.height] Height of the start of the region
+     * @param {Integer} [centerX=0]             X-position of the center of the region (relative to offsetX)
+     * @param {Integer} [centerY=0]             Y-position of the center of the region (relative to offsetY)
      *
      * @requires gc.Util
      *
      * @constructor
      * @memberOf gc
-     * @version 1.0.0
+     * @version 0.2.0
      * @author @danikaze
      */
-    var TextureRegion = function(texture, x, y, w, h) {
+    var TextureRegion = function(texture, offsetX, offsetY, width, height, centerX, centerY) {
 
         //////////////////////////
         // PUBLIC INSTANCE VARS //
@@ -40,17 +42,17 @@
         /** Texture source of the image. */
         this.texture = texture;
         /** x-position of the start of the region. */
-        this.offsetX = x;
+        this.offsetX = offsetX || 0;
         /** y-position of the start of the region. */
-        this.offsetY = y;
+        this.offsetY = offsetY || 0;
         /** Width of the region */
-        this.width = w;
+        this.width = width || texture.width;
         /** Height of the region */
-        this.height = h;
-        /** x-position of the center of the region */
-        this.centerX = 0;
-        /** y-position of the center of the region */
-        this.centerY = 0;
+        this.height = height || texture.height;
+        /** x-position of the center of the region (relative to offsetX) */
+        this.centerX = centerX || 0;
+        /** y-position of the center of the region (relative to offsetY) */
+        this.centerY = centerY || 0;
 
         ////////////////////
         // PUBLIC METHODS //
@@ -96,14 +98,14 @@
                 case 9: // draw(ctx, sx, sy, sw, sh, dx, dy, dw, dh)
                     ctx.drawImage(
                         this.texture,
-                        this.offsetX + arguments[1],
-                        this.offsetY + arguments[2],
-                        Math.min(arguments[3], this.width - arguments[1]),
-                        Math.min(arguments[4], this.height - arguments[2]),
-                        arguments[5] - this.centerX * (arguments[7]/this.width),
-                        arguments[6] - this.centerY * (arguments[8]/this.height),
-                        arguments[7],
-                        arguments[8]
+                        this.offsetX + arguments[1],    // sx
+                        this.offsetY + arguments[2],    // sy
+                        Math.min(arguments[3], this.width - arguments[1]),  // sw
+                        Math.min(arguments[4], this.height - arguments[2]), // sh
+                        arguments[5] - this.centerX * (arguments[7]/this.width),    // dx
+                        arguments[6] - this.centerY * (arguments[8]/this.height),   // dy
+                        arguments[7],   // dw
+                        arguments[8]    // dh
                     );
                     break;
 
